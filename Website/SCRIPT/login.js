3a690c99a412fd5d337320cf0b792cc01a48a194
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-01-02 02:22:07 +0530

    login page changed

--- Website/SCRIPT/login.js
+++ Website/SCRIPT/login.js
@@ -1,53 +1,27 @@
 var login__btn = document.querySelector(".login__btn")
 login__btn.addEventListener("click", validation)
 
-var check = async() => {
-    var url = "http://localhost:3000/user"
-
-    var req_response = await fetch(url)
-    var data = await req_response.json()
+function validation(){
 
     var user = document.querySelector(".username").value
     var email = document.querySelector(".email").value
     var password = document.querySelector(".password").value
-    var flag = false
 
-    for(var i = 0; i < data.length; i++)
-    {
-        if(data[i].username == user && data[i].email == email && data[i].password == password)
-        {
-            console.log(data[i].id)
-            var user_url = `http://localhost:3000/user/${data[i].id}`
-            var obj = {
-                id : data[i].id,
-                username : data[i].username,
-                email : data[i].email,
-                password : data[i].password,
-                flag : true
-            } 
-            var objString = JSON.stringify(obj)
-            var sendflag = async() => {
-                try{
-                    var req_response = await fetch(user_url, {method: "PUT", body : objString, headers : {"Content-Type" : "application/json; charset = utf-8"}})
-                }
-                catch(error){
-                    console.log(error)
-                }
-                
-            }
-            sendflag()
-            window.location.href = "../HTML/timer.html"
-            flag = true
-            break;
-        }
+    var obj = {
+        username : user,
+        email : email,
+        password : password
     }
-    if(!flag)
-    {
-        window.alert("Invalid Username or Password")
-    }
-}
+    var objString = JSON.stringify(obj)
 
-function validation(){
-    check()
+    if(user == "" || email == "" || password == "")
+    {
+        window.alert("Invalid inputs")
+    }
+    else
+    {
+        localStorage.setItem("user", objString)
+        window.location = "../HTML/timer.html"
+    }
 }
 
